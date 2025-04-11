6bac171c8340bd24310b40a0e0e465742b3913c0
Abhinav Parameshwaran <104693370+Beckendrof@users.noreply.github.com>
2025-04-10 20:55:00 -0700

    URL path

--- app/main.go
+++ app/main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+func do(conn net.Conn) {
+	buff := make([]byte, 1024)
+	_, err := conn.Read(buff)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Parse the HTTP request
+	request := string(buff)
+	parts := strings.Split(request, "\r\n")
+
+	path := strings.Split(strings.Split(parts[0], "/")[1], " ")[0]
+	var response string
+	if strings.Contains(path, ".html") || path == "" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	conn.Write([]byte(response))
+
+	conn.Close()
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,12 +44,12 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
 	for {
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Failed to accept connection")
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		go do(conn)
 	}
 }
